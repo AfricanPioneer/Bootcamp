65de313cbceff409155a905c4ab9c462f2612cde
macharianyagah <petermacharia26@gmail.com>
2016-10-12 19:31:40 +0300

    find the missing number in a list

--- Bootcamp Day 1/quickstart.py
+++ Bootcamp Day 1/quickstart.py
@@ -1,9 +1,9 @@
-from __future__ import print_function
 
-import discovery
+from __future__ import print_function
 import httplib2
 import os
 
+from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
--- Bootcamp Day 3/find_missing.py
+++ Bootcamp Day 3/find_missing.py
@@ -1,13 +1,13 @@
 def find_missing(list_a, list_b):
-  if len(list_a) == 0 and len(list_b) == 0:
+  if len(list_a) == 0 and len(list_b) == 0:#Compares both lists length to determine if they are empty
     return 0
-  elif len(list_a) == len(list_b):
+  elif len(list_a) == len(list_b):#Compares both lists length to determine if they are of equal length
     for num in list_a:
       if num in list_b:
         return 0
   else:
-    if len(list_a) < len(list_b):
+    if len(list_a) < len(list_b):#Compares both lists length to determine if they have the same number of values
       for num in list_b:
         if num not in list_a:
-          return num
+          return num # if not it returns the extra number in the list
 
