cff31048d12fbb36cbea3ed34a81e1ad63a04acd
macharianyagah <petermacharia26@gmail.com>
2016-10-13 08:21:02 +0300

    A function that generates fibonacci series

--- Bootcamp Day 1/fibonacci.py
+++ Bootcamp Day 1/fibonacci.py
@@ -1,12 +1,26 @@
+#Function to list fibonacci series
 def fibonacci(n):
-    a = 0
-    b = 1
-    for i in range(0, n):
-     temp = a
-     a = b
-     b = temp + b
-    return a
 
-num = input("Please enter a number to get its fibonacci series ")
-for c in range(0, num):
-    print(fibonacci(c))
+    if n == 0:
+     return 0
+    elif type(n) is not int:
+        raise TypeError
+    elif n<0:
+        raise ValueError
+
+     # Initialize first two fibonacci numbers
+    fib1 = 0
+    fib2 = 1
+    lst = []
+
+    # Add the last number and second last number
+    for i in range(n):
+        lst.append(fib1)
+        nextNum = fib1 + fib2
+        fib2 = fib1
+        fib1 = nextNum
+    return(lst)
+
+#num = input("Please enter a number to get its fibonacci series ")
+#for c in range(0, num):
+ #   print(fibonacci(c))
